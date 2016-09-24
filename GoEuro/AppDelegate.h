0658a817e38049debca94147ffc537357ecdaa35
Konstantin <kbuump@ya.ru>
2016-09-24 18:00:08 +0300

    Initial Commit

---
+++ GoEuro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoEuro
+//
+//  Created by Konstantin on 24.09.16.
+//  Copyright © 2016 Konstantin Skoblik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoEuro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoEuro
+//
+//  Created by Konstantin on 24.09.16.
+//  Copyright © 2016 Konstantin Skoblik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
